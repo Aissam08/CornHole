3f2850d934526e0f7c50fa5ab161be5905f7b387
= <rabhi.aissam@hotmail.fr>
2021-06-08 09:15:11 +0200

    color filter

--- src/contours.py
+++ src/contours.py
@@ -1,15 +1,18 @@
 import numpy as np
 import argparse
 import cv2 as cv
+import os
 
+#-- Lecture d'image
 
-#-- Image reading
-
-img = cv.imread("/home/aissam/Stage/CornHole/blue_hole.jpeg")
+#os.system("rm *.png")
+print(np.pi)
+img = cv.imread("/home/aissam/Stage/CornHole/img/blue_hole3.jpeg")
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 h,s,v = cv.split(hsv)
-ret_h, th_h = cv.threshold(s,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
+ret_h, th_h = cv.threshold(h,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
 ret_s, th_s = cv.threshold(s,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
+ret_v, th_v = cv.threshold(v,0,255,cv.THRESH_BINARY + cv.THRESH_OTSU)
 
 #-- Fusionner les bords
 th = cv.bitwise_or(th_h,th_s)
@@ -39,9 +42,10 @@ contours, hierarchy = cv.findContours(th, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 print(len(contours))
 for i in range(len(contours)):
     mask_BB_i = np.zeros((len(th), len(th[0])), np.uint8)
-    x,y,w,h   = cv.boundingRect(contours[i])
-    cv.drawContours(mask_BB_i, contours, i, (255,255,255), -1)
-    BB_i=cv.bitwise_and(img,img,mask=mask_BB_i)
-    if h>25 and w>25:
-        BB_i=BB_i[y:y+h,x:x+w]
-        cv.imwrite("BB_"+str(i)+".png", BB_i)
+    circularity = 4*np.pi
+    #x,y,w,h   = cv.boundingRect(contours[i])
+    #cv.drawContours(mask_BB_i, contours, i, (255,255,255), -1)
+    #BB_i=cv.bitwise_and(img,img,mask=mask_BB_i)
+    #if h>10 and w>10 and h < 40 and w < 40:
+    #    BB_i=BB_i[y:y+h,x:x+w]
+    #    cv.imwrite("BB_"+str(i)+".png", BB_i)
